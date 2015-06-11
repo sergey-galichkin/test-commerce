efd1128c8d8c44e1873e9040b2ab2fcce52d66a0
Sergey Krutous <skrutous@createthegroup.com>
2015-06-11 15:14:27 +0300

    Fixing tabs in home controller spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe "GET start page" do
-  	it "has a 200 status code" do
+    it "has a 200 status code" do
       get :index
       expect(response.status).to eq(200)
-  	end
-    
+    end
+
     it "renders index template" do
       get :index
       expect(response).to render_template("index")
