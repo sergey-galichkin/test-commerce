466bafef04a7d72055c2fc530f6f95d1332771b1
Sergey Krutous <skrutous@createthegroup.com>
2015-06-11 17:19:52 +0300

    Fixing tabs in home controller spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe HomeController, type: :controller do
   describe "GET #index" do
     subject { get :index }
 
-  	it { is_expected.to have_http_status(:ok) }
+    it { is_expected.to have_http_status(:ok) }
     
     it { is_expected.to render_template("index") }
   end
