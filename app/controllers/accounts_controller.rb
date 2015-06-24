6e999816d252ee5f22141de11ab93ef62a8cc25e
Alexander Ivanov <Alexander.Ivanov@teknavo.com>
2015-06-24 14:50:09 +0300

    DRY-ed accounts_controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,29 +5,19 @@ class AccountsController < ApplicationController
 
   # POST /accounts
   def create
-    subdomain = account_params[:subdomain]
-    return render :new if subdomain_exists? subdomain
-
-    token = SecureRandom.uuid
-
-    role = Role.find_by name: 'AccountOwner'
     account = Account.new account_params
-    account.registration_token = token
+    account.registration_token = SecureRandom.uuid
+    return render :new until account.save
+
     begin
-      if account.save
-        Apartment::Tenant.switch!(subdomain)
-        user = User.create(email: user_params[:email], password: user_params[:password], role_id: role.id)
-        if user.persisted?
-          redirect_to url_for(action: :login_with_token, subdomain: subdomain, token: token, email: user_params[:email])
-        else
-          account.destroy
-          Apartment::Tenant.reset
-          render :new
-        end
-      else
-        render :new
-      end
-    rescue => e
+      Apartment::Tenant.switch!(account.subdomain)
+
+      user = User.new user_params
+      user.role = Role.find_by_name 'AccountOwner'
+      user.save!
+
+      redirect_to url_for(action: :login_with_token, subdomain: account.subdomain, token: account.registration_token, email: user.email)
+    rescue
       account.destroy
       Apartment::Tenant.reset
       render :new
@@ -37,12 +27,12 @@ class AccountsController < ApplicationController
   # GET /accounts/new/login_with_token
   def login_with_token
     return handle_wrong_login_params unless login_params_present?
-    return handle_wrong_login_params unless login_params_valid?
 
-    account = Account.find_by(registration_token: params.require(:token), subdomain: Apartment::Tenant.current)
-    account.update(registration_token: nil)
+    account = Account.find_by_registration_token_and_subdomain params[:token], Apartment::Tenant.current
+    user = User.find_by_email params[:email]
+    return handle_wrong_login_params if account.nil? || user.nil?
 
-    user = User.find_by(email: params.require(:email))
+    account.update registration_token: nil
     sign_in user, bypass: true
 
     render :account_created
@@ -58,20 +48,8 @@ class AccountsController < ApplicationController
     params.require(:account).permit(:email, :password)
   end
 
-  def login_with_token_params
-    params.permit(:email, :token)
-  end
-
-  def subdomain_exists?(subdomain)
-    Account.exists?(subdomain: subdomain)
-  end
-
   def login_params_present?
     params.key?(:email) && params.key?(:token)
-  end
-
-  def login_params_valid?
-    User.exists?(email: params.required(:email)) && Account.exists?(registration_token: params.required(:token))
   end
 
   def handle_wrong_login_params
