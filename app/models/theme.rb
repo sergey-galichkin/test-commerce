e8a6165168611fc1ce6dc6e1edb189c145e78a82
Sergey Krutous <skrutous@createthegroup.com>
2015-06-25 10:51:02 +0300

    shortening validations of theme and theme_status models

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -2,10 +2,9 @@ class Theme < ActiveRecord::Base
   NAME_LIMIT = 100
   ZIP_FILE_URL_LIMIT = 2000
 
-  validates_presence_of :name, :zip_file_url, :theme_status
-  validates_length_of :name, maximum: NAME_LIMIT
-  validates_length_of :zip_file_url, maximum: ZIP_FILE_URL_LIMIT
-  validates_uniqueness_of :name, :zip_file_url, case_sensitive: false
-
+  validates :name, presence: true, length: { maximum: NAME_LIMIT }, uniqueness: { case_sensitive: false }
+  validates :zip_file_url, presence: true, length: { maximum: ZIP_FILE_URL_LIMIT }, uniqueness: { case_sensitive: false }
+  validates_presence_of :theme_status
+  
   belongs_to :theme_status
 end
--- app/models/theme_status.rb
+++ app/models/theme_status.rb
@@ -1,6 +1,4 @@
 class ThemeStatus < ActiveRecord::Base
   NAME_LIMIT = 20
-  validates_presence_of :name
-  validates_length_of :name, maximum: NAME_LIMIT
-  validates_uniqueness_of :name, case_sensitive: false
+  validates :name, presence: true, length: { maximum: NAME_LIMIT}, uniqueness: { case_sensitive: false }
 end
