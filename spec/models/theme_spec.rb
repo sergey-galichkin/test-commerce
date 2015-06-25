b0dd7eb406871d484bbf9928608b691c09a0a9d5
Sergey Krutous <skrutous@createthegroup.com>
2015-06-25 17:44:11 +0300

    adding nullability tests of theme and theme_status model columns

--- spec/models/theme_spec.rb
+++ spec/models/theme_spec.rb
@@ -3,12 +3,12 @@ RSpec.describe Theme, type: :model do
     it {is_expected.to validate_presence_of :name}
     it {expect(build(:theme)).to validate_uniqueness_of(:name).case_insensitive }
     it {expect(build(:theme)).to validate_length_of(:name).is_at_most Theme::NAME_LIMIT }
-    it {is_expected.to have_db_column(:name).with_options limit: Theme::NAME_LIMIT}
+    it {is_expected.to have_db_column(:name).with_options limit: Theme::NAME_LIMIT, null: false }
 
     it {is_expected.to validate_presence_of :zip_file_url}
     it {expect(build(:theme)).to validate_uniqueness_of(:zip_file_url).case_insensitive}
     it {is_expected.to validate_length_of(:zip_file_url).is_at_most Theme::ZIP_FILE_URL_LIMIT }
-    it {is_expected.to have_db_column(:zip_file_url).with_options limit: Theme::ZIP_FILE_URL_LIMIT}
+    it {is_expected.to have_db_column(:zip_file_url).with_options limit: Theme::ZIP_FILE_URL_LIMIT, null: false }
 
     it {is_expected.to belong_to :theme_status}
     it {is_expected.to validate_presence_of :theme_status}
