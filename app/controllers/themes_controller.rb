a4a916252be0f0e82fe6fd9706df457fcd2cac14
Alexander Ivanov <Alexander.Ivanov@teknavo.com>
2015-07-17 12:20:21 +0300

    DRY-ed themes_controller

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -16,8 +16,8 @@ class ThemesController < ApplicationController
     key = params.require(:key)
     theme_file_name = key.sub(/[^_]*_/, '') # theme zip file name (theme name) is located after the first _ (underscore)
     theme_name =  File.basename theme_file_name, File.extname(theme_file_name)
-    theme = Theme.create name: theme_name, zip_file_url: key, status: :processing
-    if theme.valid?
+    theme = Theme.new name: theme_name, zip_file_url: key, status: :processing
+    if theme.save
       TransferThemeJob.perform_later theme
       redirect_to action: :index
     else
